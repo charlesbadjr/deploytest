0ae989ec42b3f75ee2248e37f6d0cab2ea7a2e87
Charlie <charlesbadjr@gmail.com>
2018-11-27 11:29:46 -0700

    first deploy

---
+++ public/precache-manifest.ae25b90ab05fa03506e88c922d4e9099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1de6a1b8e48797484299",
+    "url": "/static/js/main.1de6a1b8.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "1de6a1b8e48797484299",
+    "url": "/static/css/main.8881d45d.chunk.css"
+  },
+  {
+    "revision": "c5f0182e3cebcca596280af07ff671ca",
+    "url": "/index.html"
+  }
+];
